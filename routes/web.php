5513695c2db19e7c169bbcd9c15e31075c37108b
lalala_shuntaro <lalala.shuntaro.8.24@gmail.com>
2021-02-03 21:12:37 +0900

    sass-success

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\HobbyController@index');
+
+// Route::get('/', function(){
+//     return view('hobby.index');
+// });
+
