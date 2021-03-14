ecef42312028a278780cf6efe9470f19b72a17dd
lalala_shuntaro <lalala.shuntaro.8.24@gmail.com>
2021-03-14 21:00:01 +0900

    latest-file

--- resources/views/components/newheader.blade.php
+++ resources/views/components/newheader.blade.php
@@ -1,8 +1,20 @@
-<div class="header">
-    <div class="header-logo">
-        <a href=""> {{$logo}} </a>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="" href="{{ asset('/favicon3.ico') }}">
+    <link rel="stylesheet" href="{{ asset('/css/new.css') }}">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <title>@yield('title')</title>
+</head>
+    <div class="header">
+        <div class="header-logo">
+            <a href=""> {{$logo}} </a>
+        </div>
+        <div class="login-button">
+            <a href="/login"> {{$login}} </a>
+        </div>
     </div>
-    <div class="login-button">
-        <a href="/login"> {{$login}} </a>
-    </div>
-</div>
+</html>
--- resources/views/layouts/hobbyapp.blade.php
+++ resources/views/layouts/hobbyapp.blade.php
@@ -2,6 +2,12 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="" href="{{ asset('/favicon3.ico') }}">
+    <link rel="stylesheet" href="{{ asset('/css/login.css') }}">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <title>@yield('title')</title>
 </head>
 <body>
