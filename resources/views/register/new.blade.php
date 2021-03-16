ef60879d18c90bb000c549b9252e21672569615b
lalala_shuntaro <lalala.shuntaro.8.24@gmail.com>
2021-03-16 15:41:48 +0900

    new-file

--- resources/views/register/new.blade.php
+++ resources/views/register/new.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.hobbynew')
 @section('main')
-<div class="login-name">
-    仮会員登録
-</div>
-<form action="/confirmation" method="post">
-    @csrf
-    <div class="control">
-        <input type="text" placeholder="名前" style="padding-left: 15px;" name="name">
+    <div class="login-name">
+        仮会員登録
     </div>
+    <form action="/confirmation" method="post">
+        @csrf
+        <div class="control">
+            <input type="text" placeholder="名前" style="padding-left: 15px;" name="name">
+        </div>
 
-    <div class="control">
-        <input type="mail" placeholder="メールアドレス" style="padding-left: 15px;" name="email">
-    </div>
+        <div class="control">
+            <input type="mail" placeholder="メールアドレス" style="padding-left: 15px;" name="email">
+        </div>
 
-    <div class="control">
-        <input type="password" placeholder="パスワード" style="padding-left: 15px;" name="pasword" >
-    </div>
+        <div class="control">
+            <input type="password" placeholder="パスワード" style="padding-left: 15px;" name="password" >
+        </div>
 
-    <div class="control-button">
-        <button>登録</button>
-    </div>
-</form>
+        <div class="control-button">
+            <button>登録</button>
+        </div>
+    </form>
 @endsection
